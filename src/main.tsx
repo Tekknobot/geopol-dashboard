5c999bf5d92bf18b349e3bf578bb74fcf2beeef2
Tekknobot <thrillatron@hotmail.com>
2026-04-19 14:49:05 -0400

    Navigation fix.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import "leaflet/dist/leaflet.css";   // if you’re not relying on the <link> in index.html
@@ -9,8 +9,8 @@ import "./leaflet-icons";            // the file above
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
